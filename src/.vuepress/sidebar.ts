907d8481a6f72b1493c14949d56a2e74010d50b6
mcenahle <admin@mcenahle.sh.cn>
2026-04-29 08:41:42 +0800

    style: 隔离显示数学的侧边栏和其它侧边栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,34 +1,37 @@
-import { sidebar } from "vuepress-theme-hope";
+import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    "me",
-    {
-      text: "我的自我介绍",
-      icon: "carbon:user-profile-alt",
-      prefix: "my/",
-      link: "my/",
-      children: "structure",
-    },
-    {
-      text: "代码规范",
-      icon: "material-symbols:code",
-      prefix: "coding-standards/",
-      link: "coding-standards/",
-      children: ["github-commit-signature", "write-commit-message"],
-    },
-    {
-      text: "数学",
-      icon: "boxicons:math",
-      prefix: "math/",
-      link: "math/",
-      children: ["2026-04-27-exercise", "2026-04-27-study", "2026-04-28-study"],
-    },
-    {
-      text: "上海师范大学",
-      icon: "mdi:university",
-      link: "https://www.shnu.edu.cn",
-    },
-  ],
+    "/": [
+        "",
+        "me",
+        {
+            text: "我的自我介绍",
+            icon: "carbon:user-profile-alt",
+            prefix: "my/",
+            link: "my/",
+            children: "structure",
+        },
+        {
+            text: "代码规范",
+            icon: "material-symbols:code",
+            prefix: "coding-standards/",
+            link: "coding-standards/",
+            children: ["github-commit-signature", "write-commit-message"],
+        },
+        {
+            text: "上海师范大学",
+            icon: "mdi:university",
+            link: "https://www.shnu.edu.cn",
+        },
+    ],
+    "/math/": [
+        {
+            text: "数学",
+            icon: "boxicons:math",
+            prefix: "",
+            collapsible: true,
+            expanded: true,
+            children: ["2026-04-27-exercise", "2026-04-27-study", "2026-04-28-study"],
+        },
+    ],
 });
