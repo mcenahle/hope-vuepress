e2e35e9830ecb8bfa6048557f89a351c773fbeb8
mcenahle <admin@mcenahle.sh.cn>
2026-05-09 19:28:00 +0800

    style: 导航栏设置代码迁移到 theme.ts

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,34 +1,15 @@
-import { navbar } from "vuepress-theme-hope";
-
+import {navbar} from "vuepress-theme-hope";
+// 已被迁移到 theme.ts
 export default navbar([
-  "/",
-  "/me",
-  "/my/",
-  "coding-standards/",
-  /* {
-    text: "代码规范",
-    icon: "material-symbols:code",
-    prefix: "/coding-standards/",
-    children: [
-      {
-        text: "Github 相关",
-        icon: "mdi:github",
-        prefix: "/coding-standards/",
-        children: ["github-commit-signature", "write-commit-message"],
-      },
-      {
-        text: "编程语言相关",
-        icon: "akar-icons:python-fill",
-        prefix: "/",
-        children: ["https://peps.python.org/pep-0008/", "https://ecma-international.org/publications-and-standards/standards/ecma-262/"],
-      }
-    ],
-  }, */
+    "/",
+    "/me",
+    "/my/",
+    "coding-standards/",
     "/math/",
-  {
-    text: "个人成长记录",
-    icon: "mingcute:web-fill",
-    link: "https://mcenahle.cn/",
-  },
-  "/friendslink",
+    {
+        text: "个人成长记录",
+        icon: "mingcute:web-fill",
+        link: "https://mcenahle.cn/",
+    },
+    "/friendslink",
 ]);
