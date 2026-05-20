731e18f4f555fdf71c43acc7ce2aca9f3878562c
mcenahle <admin@mcenahle.sh.cn>
2026-05-20 19:13:36 +0800

    style: 修改“预览”按钮样式为蓝色样式

--- src/.vuepress/public/js/custom.js
+++ src/.vuepress/public/js/custom.js
@@ -1,17 +1,16 @@
-const timer = setInterval(() => {
-    const btn = document.querySelector(
-        'button.el-button.tk-send'
-    );
-    if (btn) {
-        btn.remove();
-        clearInterval(timer);
-    }
-}, 300);
-
 setInterval(() => {
+    // 删除“发送”按钮
     document.querySelectorAll("button").forEach(btn => {
         if (btn.innerText.trim() === "发送") {
             btn.remove();
         }
     });
-}, 300);
+
+    // 修改“预览”按钮样式
+    document.querySelectorAll("button").forEach(btn => {
+        if (btn.innerText.trim() === "预览") {
+            btn.classList.remove("el-button--default");
+            btn.classList.add("el-button--primary");
+        }
+    });
+}, 30);
