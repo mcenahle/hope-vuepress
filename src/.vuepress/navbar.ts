1cc634310778ff2423afad36c4718af546a9fdf4
mcenahle <hi@mcenahle.page>
2026-07-05 20:56:46 +0800

    feat: 重新整理、组织导航

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,22 +2,25 @@ import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
     "/",
-    "/me",
-    "/my/",
     {
-        text: "我的爱好",
-        icon: "mdi:heart",
-        children: ["coding-standards/", "edu/", "med/", "/status/", "resources/"],
+        text: "我",
+        icon: "mdi:face-man-profile",
+        children: ["/me", "my/", "coding-standards/", "edu/", "med/", "/status/", "resources/"],
     },
     "/friendslink",
     {
-        text: "个人成长记录",
-        icon: "mingcute:web-fill",
-        link: "https://mcenahle.cn/",
+        text: "博客网站",
+        icon: "mdi:post-it-note-text-outline",
+        link: "https://blog.mcenahle.page/",
     },
     {
         text: "电子邮件系统",
         icon: "mdi:email-outline",
         link: "https://mail.mcenahle.page/",
+    },
+    {
+        text: "个人成长记录",
+        icon: "mingcute:web-fill",
+        link: "https://mcenahle.cn/",
     },
 ]);
